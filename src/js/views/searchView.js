9549cc75e580d0b23566d4c5b468fd874405caf3
simon <simonfisher227@gmail.com>
2022-11-27 17:07:03 +0800

    docs: add mvc practice

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,6 +2,7 @@ class SearchView {
   _parentElment = document.querySelector('.search');
   _searchFieldEl = document.querySelector('.search').querySelector('.search__field');
 
+  // 对外暴露获取输入框的值的方法，什么时候调用由外界决定
   getInput() {
     const input = this._searchFieldEl.value;
     this._clearInput();
@@ -18,7 +19,7 @@ class SearchView {
       // 表单提交事件阻止默认行为，防止页面刷新
       e.preventDefault();
       handler();
-    })
+    });
   }
 }
 
